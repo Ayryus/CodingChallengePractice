7dfb1fc523c5a28e44e354006a99b2a8fd6a2d9f
Ayryus <35973756+Ayryus@users.noreply.github.com>
2018-02-26 12:45:28 -0700

    Add files via upload

--- Festival.java
+++ Festival.java
@@ -1,46 +1,43 @@
-import java.util.ArrayList;
- 
+import java.util.*;
+
 public class Festival {
+	private String name;
+	private ArrayList<Movie> movieList;
 	
-	String name;
-	ArrayList<Movie> Movie = new ArrayList<Movie>();
-	
-	public Festival(String name) {
-		
-		this.name = this.name;
+	public Festival(String newName){
+		name = newName;
+		movieList = new ArrayList<Movie>();
 	}
 	
-	
-	public void addMovie(Movie newMovie) {
-		
-		
-		
-		
-	}
-	
-	public Movie getMovieWithLowestRating() {
-		
-		
-		
-	
-	}
-	
-	public ArrayList<Movie> getMovieList() {
-		
-		
-		
-	}
-	
-	
-	public String getName() {
-		
-		
+	public String getName(){
 		return name;
-		
 	}
 	
+	public ArrayList<Movie> getMovieList(){
+		ArrayList<Movie> newlist = new ArrayList<Movie>();
+		for(int count= 0; count < movieList.size(); count++)
+			newlist.add(new Movie(movieList.get(count)));
+		return newlist;
+	}
 	
+	public void addMovie(Movie m){
+		
+		movieList.add(new Movie(m));
+	}
 	
-	
-	
+	public Movie getMovieWithLowestRating(){
+		Movie lowestRating;
+		
+		if (movieList.size() == 0)
+			return null;
+		else{
+			lowestRating = movieList.get(0);
+			for(int count= 1; count < movieList.size(); count++){
+					if(movieList.get(count).getRating() < lowestRating.getRating())
+						lowestRating = movieList.get(count);
+			}
+			
+		}
+		return new Movie(lowestRating);
+	} 
 }
